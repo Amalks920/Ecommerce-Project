6bef4532f7daa9987293e1f52df187ca8d56145f
Amalks920 <Amalks920@gmail.com>
2023-07-10 23:05:05 +0530

    mongodb connection established

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express=require('express');
 const app=express();
 const dotenv=require('dotenv').config();
+const dbConnect=require('./config/dbConnect')
 const PORT=process.env.PORT || 4000
 
+dbConnect()
 
+app.use('/',(req,res,next)=>{
+    res.send(`<h1>Hello from Server</h1>`)
+})
 
 
 
