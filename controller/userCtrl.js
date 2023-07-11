b70bd4114316eb5f1382e7901f62db0fd90ee730
Amalks920 <Amalks920@gmail.com>
2023-07-11 12:35:01 +0530

    validation added in user schema

--- controller/userCtrl.js
+++ controller/userCtrl.js
@@ -1,16 +1,21 @@
 const User=require('../models/userSchema')
 
 const createUser=async(req,res)=>{
-    console.log('hi')
+
     const email=req.body.email;
     console.log(req.body.email)
     const findUser=await User.findOne({email:email})
-    console.log(findUser)
+   
     if(!findUser){
-       
-        
-    User.create(req.body)
-         res.json({response:"success"})
+
+        try {
+        let user=await User.create(req.body)
+            res.json({user:user})
+        } catch (error) {
+            res.json({error:error.message})
+        }
+    
+         
     }else{
         console.log('hello')
         res.json({
--- models/userSchema.js
+++ models/userSchema.js
@@ -5,32 +5,53 @@ var userSchema = new mongoose.Schema({
     firstname:{
         type:String,
         required:true,
-
+        pattern: /^[a-zA-Z ]+$/,
+        message: 'Name must be at least 3 characters long and contain only letters and spaces',
     },
     lastname:{
         type:String,
         required:true,
+        minLength: 3,
+        maxLength: 50,
+        pattern: /^[a-zA-Z ]+$/,
+        message: 'Name must be at least 3 characters long and contain only letters and spaces',
     },
     username:{
         type:String,
         required:true,
-        unique:true
+        unique:true,
+        minLength: 3,
+        maxLength: 50,
+        pattern: /^[a-z0-9]+$/,
+        message: 'Username must be at least 3 characters long and contain only lowercase letters and numbers',
     },
 
     email:{
         type:String,
         required:true,
         unique:true,
+        match: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-z]{2,}$/,
+        message: 'Invalid email address',
     },
     mobile:{
         type:String,
         required:true,
         unique:true,
+        match: /^\d{10}$/,
+        message: 'Invalid mobile number',
     },
     password:{
         type:String,
         required:true,
-        unique:true
+        minLength: 8,
+        maxLength: 128,
+        pattern: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@#$%^&+=])[a-zA-Z0-9@#$%^&+=]{8,128}$/,
+        message: 'Password must be at least 8 characters long and contain at least one uppercase letter, one lowercase letter, one number, and one special character',
+    },
+    isBlocked:{
+        type:Boolean,
+        required:true,
+        default:false
     }
 });
 
